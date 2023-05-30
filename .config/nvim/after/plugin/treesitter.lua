27eb68abb2bc90a6ac86f81e35e00c2ac207c3fe
Tobia Bocchi <tobiabocchi@serendipity.local>
2023-05-30 22:19:53 +0200

    Disable latex highlight

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,15 +1,16 @@
 -- Configure the plugin only if it's installed
-local success, ts = pcall(require, 'nvim-treesitter.configs')
+local success, ts = pcall(require, "nvim-treesitter.configs")
 if not success then
-    return
+	return
 end
 
-ts.setup {
-    ensure_installed = { "help", "python", "lua", "bash", "html" },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-}
+ts.setup({
+	ensure_installed = { "help", "python", "lua", "bash", "html" },
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		disable = { "latex" },
+		additional_vim_regex_highlighting = false,
+	},
+})
