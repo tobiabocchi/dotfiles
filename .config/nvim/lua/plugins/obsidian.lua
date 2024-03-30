bb9668a1574af54e7fe9b02a2e064741a2f62d9b
Tobia Bocchi <tobiabocchi@serendipity.home.arpa>
2024-03-30 01:30:10 +0100

    Improve config

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -8,22 +8,83 @@ return {
 		-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
 		"BufReadPre "
 			.. vim.fn.expand("~")
-			.. "syncthing/vaults/**.md",
-		"BufNewFile " .. vim.fn.expand("~") .. "syncthing/vaults/**.md",
+			.. "/syncthing/tobiabocchi/vaults/**.md",
+		"BufNewFile " .. vim.fn.expand("~") .. "/syncthing/tobiabocchi/vaults/**.md",
 	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
+	keys = {
+		{ "<Leader>o", "<cmd>ObsidianQuickSwitch<cr>", desc = "Open [O]bsidian Quick Switch" },
+	},
 	opts = {
 		workspaces = {
-			{ name = "personal", path = "~/syncthing/tobiabocchi/vaults/personal" },
-			{ name = "work", path = "~/syncthing/tobiabocchi/vaults/work" },
-			{ name = "uni", path = "~/syncthing/tobiabocchi/vaults/uni" },
+			{ name = "para", path = "~/syncthing/tobiabocchi/vaults/para" },
 		},
 		completion = {
 			-- Set to false to disable completion.
 			nvim_cmp = true,
 			min_chars = 2,
+		},
+		-- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+		-- way then set 'mappings = {}'.
+		mappings = {
+			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- Toggle check-boxes.
+			["<leader>ch"] = {
+				action = function()
+					return require("obsidian").util.toggle_checkbox()
+				end,
+				opts = { buffer = true },
+			},
+		},
+		new_notes_location = "~/syncthing/tobiabocchi/vaults/para/new",
+
+		-- Optional, customize how note IDs are generated given an optional title.
+		---@param title string|?
+		---@return string
+		note_id_func = function(title)
+			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+			-- In this case a note with the title 'My new note' will be given an ID that looks
+			-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+			local suffix = ""
+			if title ~= nil then
+				-- If title is given, transform it into valid file name.
+				suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+			else
+				-- If title is nil, just add 4 random uppercase letters to the suffix.
+				for _ = 1, 4 do
+					suffix = suffix .. string.char(math.random(65, 90))
+				end
+			end
+			return tostring(os.date("%Y%m%d%H%M%S")) .. "-" .. suffix
+		end,
+
+		-- Optional, customize how note file names are generated given the ID, target directory, and title.
+		---@param spec { id: string, dir: obsidian.Path, title: string|? }
+		---@return string|obsidian.Path The full path to the new note.
+		note_path_func = function(spec)
+			-- This is equivalent to the default behavior.
+			local path = spec.dir / tostring(spec.id)
+			return path:with_suffix(".md")
+		end,
+		picker = {
+			-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+			name = "telescope.nvim",
+			-- Optional, configure key mappings for the picker. These are the defaults.
+			-- Not all pickers support all mappings.
+			mappings = {
+				-- Create a new note from your query.
+				new = "<C-x>",
+				-- Insert a link to the selected note.
+				insert_link = "<C-l>",
+			},
 		},
 	},
 }
