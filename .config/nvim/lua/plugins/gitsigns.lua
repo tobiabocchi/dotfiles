c5dc29357a9bb1eef363b655c0451ff88424021f
Tobia Bocchi <tobia.bocchi@sighup.io>
2024-11-16 19:38:43 +0100

    feat: add keybindings for some gitsigns functionalities

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,12 +1,26 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
 	"lewis6991/gitsigns.nvim",
-	opts = {
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-		},
+	keys = {
+		{ "<Leader>gh", "<cmd>Gitsigns preview_hunk<cr>", desc = "[G]it preview [H]unk" },
+		{ "<Leader>gd", "<cmd>Gitsigns diffthis<cr>", desc = "[G]it [D]iff" },
+		{ "<Leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "[G]it toggle line [B]lame" },
 	},
+	opts = {},
+	--TODO: add navigation shortcut
+	--
+	-- map('n', ']c', function()
+	--   if vim.wo.diff then
+	--     vim.cmd.normal({']c', bang = true})
+	--   else
+	--     gitsigns.nav_hunk('next')
+	--   end
+	-- end)
+	--
+	-- map('n', '[c', function()
+	--   if vim.wo.diff then
+	--     vim.cmd.normal({'[c', bang = true})
+	--   else
+	--     gitsigns.nav_hunk('prev')
+	--   end
+	-- end)
 }
