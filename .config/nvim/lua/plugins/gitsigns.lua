4d049618be922c086cad54ab641538ed238a74ad
Tobia Bocchi <tobia.bocchi@sighup.io>
2024-11-16 20:24:50 +0100

    feat: add command to lazy load gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,7 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
 	"lewis6991/gitsigns.nvim",
 	keys = {
+		{ "<Leader>gs", "<cmd>Gitsigns refresh<cr>", desc = "Launch [G]it [S]igns" },
 		{ "<Leader>gh", "<cmd>Gitsigns preview_hunk<cr>", desc = "[G]it preview [H]unk" },
 		{ "<Leader>gd", "<cmd>Gitsigns diffthis<cr>", desc = "[G]it [D]iff" },
 		{ "<Leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "[G]it toggle line [B]lame" },
