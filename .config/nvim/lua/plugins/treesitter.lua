24e1d6dec43524368ca47088fb9b5f61329d91cf
Tobia Bocchi <29007647+tobiabocchi@users.noreply.github.com>
2025-10-20 15:21:00 +0200

    feat(treesitter): switch to main branch, update config format

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,18 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
 	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "bash", "cpp", "css", "html", "lua", "markdown", "python", "vimdoc" },
-			sync_install = false,
-			auto_install = true,
-			indent = { enable = true },
-			highlight = {
-				enable = true,
-				disable = { "latex" },
-				additional_vim_regex_highlighting = false,
-				-- additional_vim_regex_highlighting = { "latex" },
-			},
-		})
-	end,
+	lazy = false,
+	opts = {
+		ensure_installed = { "c", "bash", "cpp", "css", "html", "lua", "markdown", "python", "vimdoc" },
+		sync_install = false,
+		auto_install = true,
+		indent = { enable = true },
+		highlight = {
+			enable = true,
+			disable = { "latex" },
+			additional_vim_regex_highlighting = false,
+			-- additional_vim_regex_highlighting = { "latex" },
+		},
+	},
 }
