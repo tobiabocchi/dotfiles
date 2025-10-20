d27c8f20231e8ab2d0c2af76ee89a12b5e1bce9f
Tobia Bocchi <29007647+tobiabocchi@users.noreply.github.com>
2025-10-20 15:34:47 +0200

    feat(copilot): add copilot-lsp

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,11 +1,18 @@
 return {
 	"zbirenbaum/copilot.lua",
+	dependencies = { "copilotlsp-nvim/copilot-lsp" },
 	event = { "InsertEnter" },
 	opts = {
 		panel = { enabled = false },
 		suggestion = { enabled = false },
-		filetypes = {
-			markdown = false,
+		filetypes = { markdown = false },
+		nes = {
+			enabled = true,
+			keymap = {
+				accept_and_goto = "<leader>p",
+				accept = false,
+				dismiss = "<Esc>",
+			},
 		},
 	},
 }
